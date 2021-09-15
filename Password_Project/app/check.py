29c473c92183362ad18ad2c9d9d3ade14399f588
JHigz <jhiguita@spartglobal.com>
2021-09-15 21:55:39 +0100

    Creation of classes for Homepage and Password List Checker

--- Password_Project/app/check.py
+++ Password_Project/app/check.py
@@ -1,8 +1,9 @@
-#pw = ("blitz") #This will become the input
-
+#def classname  = new Pass_Check
+#classname.test
 class Pass_Check:
-    def __init__()
-    def common_pw_check(self, ):
+    #def __init__(self)
+
+    def common_pw_check(self):
         pw = input("What password would you like to check the list for? : \n")
 
         common_pw_file = open("Top 10000 Passwords.txt").read().splitlines()
@@ -10,9 +11,9 @@ class Pass_Check:
             print ("This password has been marked as a common password")
         else:
             print("This password does not appear to be a common password, please check your password policy before you use it ")
-        cpc_retest(self)
+        self.cpc_retest()
 
-    def cpc_retest(self, ):
+    def cpc_retest(self):
         try:
             print("1. Retest another password")
             print("2. Go back to menu")
@@ -24,11 +25,13 @@ class Pass_Check:
                 print("2. Go back to menu")
                 retest = int(input("What would you like to do from the above options? :\n"))
             if retest == 1:
-                common_pw_check(self)
+                self.common_pw_check()
             elif retest == 2:
                 print("this should go back to menu")
+
+
         except ValueError:
             print()
             print ("You have not selected a valid option, please re-enter your choice :\n")
             print()
-            cpc_retest(self)
+            self.cpc_retest()
--- Password_Project/app/startup.py
+++ Password_Project/app/startup.py
@@ -1,44 +1,49 @@
 from check import Pass_Check
+play=Pass_Check()
 
 
-def welcome():
-    print("Welcome to the password Analyser/Generator")
-    print()
-    print("1. Password Analyser")
-    print("2. Password Generator")
-    print("3. Exit Application")
-    print()
+class Homepage:
+    def welcome(self):
+        print("Welcome to the password Analyser/Generator")
+        print()
+        print("1. Password Analyser")
+        print("2. Password Generator")
+        print("3. Exit Application")
+        print()
 
-def function_choice():
-    try:
-        function = int(input("Please choose one of the above options to select the function you would like to use :\n"))
-
-        while function not in [1,2,3]:
-        #while function > 3 or function < 1 or:
-            print()
-            print("You have not selected a valid option, please re-enter your choice :\n")
+    def function_choice(self):
+        try:
             function = int(input("Please choose one of the above options to select the function you would like to use :\n"))
 
-        if function == 1:
-            #run password analyser
-            print ("This was option 1")
-            Pass_Check.common_pw_check(self)
+            while function not in [1,2,3]:
+            #while function > 3 or function < 1 or:
+                print()
+                print("You have not selected a valid option, please re-enter your choice :\n")
+                function = int(input("Please choose one of the above options to select the function you would like to use :\n"))
 
-        elif function == 2:
-            #run password generator
-            print ("This was option 2")
+            if function == 1:
+                #run password analyser
+                print ("This was option 1")
+                play.common_pw_check()
+                #Pass_Check.common_pw_check(self)
 
-        elif function == 3:
+            elif function == 2:
+                #run password generator
+                print ("This was option 2")
+
+            elif function == 3:
+                print()
+                exit("Thank you for using the app")
+
+        except ValueError:
             print()
-            exit("Thank you for using the app")
+            print ("You have not selected a valid option, please re-enter your choice :\n")
+            print()
+            self.function_choice()
 
-    except ValueError:
-        print()
-        print ("You have not selected a valid option, please re-enter your choice :\n")
-        print()
-        function_choice()
-
-welcome()
-function_choice()
+            
+home=Homepage()
+home.welcome()
+home.function_choice()
 print()
 print("The code ran all the way")
